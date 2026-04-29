719ba2a294b3467d99a77afacbe6af8dd20d32c6
Raine <kalelmcevoy@hotmail.com>
2026-04-29 23:55:11 +0100

    ATLAS AND LAGRANGE CAN NOW COMMUNICATE THIS MEANS THAT THE API CAN GET DATA LIKE USERS AND MESSAGES atlas lagrange connection prototype lagrange api calls can now get data from atlas' database and return them this is a very rudimentary implementation and will be changed later on down the line

--- src/lib/atlas/AtlasInterface.ts
+++ src/lib/atlas/AtlasInterface.ts
@@ -1,16 +1,15 @@
-import { createServer, Server } from "net";
+import { createServer, Server, Socket, connect } from "net";
 import { Logger, LogLevel } from "../../../../Common/Logging/dist/Logger.js";
 import { unlinkSync } from "fs";
 
 // todo: cleanup and make proper service
 
-export function connectAtlas(): Server {
-    const sock = createServer();
-    sock.listen("/tmp/WyvernSockets/Atlas", () => {
-        console.log("Connected");
-    })
-    sock.on("message", (msg) => {
-        Logger.sendLog(LogLevel.Verbose, ["ATLAS", "Socketing", "Lagrange-Atlas"], msg.toJSON());
+export function connectAtlas(): Socket {
+    const sock = connect({
+        path: "/tmp/WyvernSockets/Atlas",
+    });
+    sock.on("ready", ()=>{
+        console.log("lagrange ready")
     })
 
     process.on("SIGINT", (code) => {
@@ -21,4 +20,24 @@ export function connectAtlas(): Server {
 
     return sock
 
+}
+
+export const AtlasSocket = connectAtlas();
+
+AtlasSocket.on("data", (msg) => {
+    Logger.sendLog(LogLevel.Verbose, ["ATLAS", "Socketing", "Lagrange-Atlas"], JSON.parse(msg as string));
+})
+enum AtlasRequestType {
+    GetUserByID
+}
+interface AtlasRequest {
+    type: AtlasRequestType
+    data: {}
+}
+
+export function messageAtlas(data: AtlasRequest, callback: (res: any) => any) {
+    AtlasSocket.write(JSON.stringify(data));
+    AtlasSocket.once("data", (msg: string) => {
+        callback(msg);
+    })
 }
