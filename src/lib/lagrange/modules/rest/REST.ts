c6846717086d8ea9ed1e84d9cf9a28dc8c881077
Raine <kalelmcevoy@hotmail.com>
2026-05-05 11:49:12 +0100

    this didnt sho up on the staged changed thanks

--- src/lib/lagrange/modules/rest/REST.ts
+++ src/lib/lagrange/modules/rest/REST.ts
@@ -1,7 +1,7 @@
 import { Logger, LogLevel } from "../../../../../../Common/Logging/dist/Logger.js";
-import { Router } from "./Router.js";
+import { LagrangeAPI } from "./Router.js";
 //import { AtlasSocket, messageAtlas } from "../../atlas/AtlasInterface.js";
 
-export const API = new Router(); // export api for the sake of interfacing in other places
+export const API = new LagrangeAPI(); // export api for the sake of interfacing in other places
 
 
