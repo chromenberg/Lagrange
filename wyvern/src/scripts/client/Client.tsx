5f80ee79294c547e7e60a3c7daa335e886f3087a
Raine <kalelmcevoy@hotmail.com>
2026-06-20 12:22:14 +0100

    feat: hardcode client to identify as account
    this token will get invalidated later

--- wyvern/src/scripts/client/Client.tsx
+++ wyvern/src/scripts/client/Client.tsx
@@ -1,11 +1,12 @@
-const IdentifyProtoBuilder = (await import("./Identify.proto")).IdentifyProtoBuilder;
+const IdentifyProtoBuilder = (await import("./Identify.proto"))
+  .IdentifyProtoBuilder;
 
 export const socket = new WebSocket("http://127.0.0.1:82");
 socket.onopen = () => {
   console.log("[Client] Connection opened to Gateway");
 };
 socket.onmessage = async (msg) => {
-  handleMessage(msg)
+  handleMessage(msg);
   /*
     Structure of message content
 
@@ -15,39 +16,41 @@ socket.onmessage = async (msg) => {
     eventName: string | undefined
   */
   // socket.send(JSON.stringify({opCode: 2}))
-
 };
-const _userData = (
-  await fetch("/api/v1/auth/register", {
-    method: "POST",
-    body: JSON.stringify({
-      username: "raine",
-      email: "raine@email.com",
-      password: "password",
-    }),
-  })
-).json();
+// const _userData = (
+//   await fetch("/api/v1/auth/register", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       username: "raine",
+//       email: "raine@email.com",
+//       password: "password",
+//     }),
+//   })
+// ).json();
 
-export const userData = await _userData
+// export const userData = await _userData;
 
 async function handleMessage(message: MessageEvent) {
-  const msg = JSON.parse(message.data)
+  const msg = JSON.parse(message.data);
   if (msg.opCode === 10) {
-    _userData.then((data) => {
-      if (data.reason) {return}
-      socket.send(IdentifyProtoBuilder(data))
-    })
+    socket.send(
+      IdentifyProtoBuilder({
+        intents: "1",
+        token:
+          "MTIyODIwNTUyNTk0MzAwOTI4.ajUhxg.8pwpnekSQXUqFAT4Fp6GKC_GVd80n0McfxNYvZz_J5M",
+      }),
+    );
+
     setInterval(() => {
-      socket.send(JSON.stringify({
-        opCode: 1,
-        data: null
-      }))
-    }, msg.data.heartbeat_interval)
+      socket.send(
+        JSON.stringify({
+          opCode: 1,
+          data: null,
+        }),
+      );
+    }, msg.data.heartbeat_interval);
   }
 
-  if (msg.eventType === "READY") {
-    
-  }
-  
+
   console.log("[Client] ", msg);
 }
