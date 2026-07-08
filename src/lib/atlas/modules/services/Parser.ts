d860194354f29101600629ab10a6fd3f74d636f1
Raine <kalelmcevoy@hotmail.com>
2026-07-08 12:29:57 +0100

    bugfix: unfuck gateway (parser copied channels many times)

--- src/lib/atlas/modules/services/Parser.ts
+++ src/lib/atlas/modules/services/Parser.ts
@@ -1,6 +1,9 @@
 import { ChannelType } from "../../../core/types/ChannelTypes.js";
 import type { WeakObj } from "../../../core/types/Types.js";
 
+function isDuplicateChannel(guild_id: any, channel: WeakObj, id: BigInt) {
+  return channel[guild_id].some((c: WeakObj) => c.id === id.toString());
+}
 export class Parser {
   constructor() {}
 
@@ -14,17 +17,25 @@ export class Parser {
       // Push all guild info into an object, keyed by guild id
       guilds[item.guild_id] = {
         id: item.guild_id?.toString(),
-        name: item.guild_name,
-        owner_id: item.owner_id?.toString(),
-        icon: item.icon_hash ?? null,
-        banner: item.banner_hash ?? null,
-        vanity_url: item.vanity_url ?? null,
+        properties: {
+          name: item.guild_name,
+          owner_id: item.owner_id?.toString(),
+          icon: item.icon_hash ?? null,
+          banner: item.banner_hash ?? null,
+          vanity_url: item.vanity_url ?? null,
+        },
       };
 
       if (!channels[item.guild_id]) {
         channels[item.guild_id] = [];
       }
       // Push all channel info into an array of objects, keyed by guild id
+      // TODO: Make a class that allows for arrays of unknown objects, but still allows
+      // for checking if a certain field is in there
+      if (isDuplicateChannel(item.guild_id, channels, item.channel_id)) {
+        return;
+      }
+
       channels[item.guild_id].push({
         id: item.channel_id.toString(),
         name: item.channel_name,
