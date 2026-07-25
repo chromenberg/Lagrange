a46d724559ec6b4a2f492316c9870fd2f567ad95
chromenberg <kalelmcevoy@hotmail.com>
2026-07-25 14:11:06 +0100

    fix(currentstore): fix event emitter leak

--- src/lib/wyvern/src/scripts/stores/current-store/CurrentStore.tsx
+++ src/lib/wyvern/src/scripts/stores/current-store/CurrentStore.tsx
@@ -1,24 +1,28 @@
-import { useEffect, useState } from "react";
+const { useEffect, useState } = await import("react");
+
 // import LocationAnnouncer from "../../core/LocationAnnouncer";
 // import type { GuildChannel } from "../../types/GuildTypes";
 import type { LocationStore } from "../../types/LocationStoreType";
-import LocationAnnouncer from "../../core/LocationAnnouncer";
-import { ChannelType } from "../../types/ChannelTypes";
+const LocationAnnouncer = (await import("../../core/LocationAnnouncer"))
+  .default;
+const { ChannelType } = await import("../../types/ChannelTypes");
 
 export function useCurrentRoute(): LocationStore {
   const [location, setLocation] = useState<LocationStore>({
-    guild: { id: null, name: null },
+    guild: { id: "INITIALIZE", name: "INITIALIZE" },
     channel: { id: null, name: null, type: ChannelType.GuildText },
   });
-
+  
   useEffect(() => {
-    // console.log("ROUTE CHANGE | GOING TO "+location.pathname)
-    // Emit the location change to the announcer
-    LocationAnnouncer.on("ROUTE_CHANGE", (data) => {
-      console.log("[Routes] Going to", "/channels/" + data.guild.id + "/" + data.channel.id)
-      console.log(data)
+    const updateLocation = (data: LocationStore) => {
+      console.log(data);
       setLocation(data);
-    });
+    }
+    LocationAnnouncer.once("ROUTE_CHANGE", updateLocation);
+
+    return () => {
+      LocationAnnouncer.remove("ROUTE_CHANGE", updateLocation)
+    };
   }, [location]);
 
   return location;
