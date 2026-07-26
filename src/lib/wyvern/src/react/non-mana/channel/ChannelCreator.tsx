6adae4ec30d8ae1ab7642008e6ad6b5d45f6d9ce
chromenberg <kalelmcevoy@hotmail.com>
2026-07-26 23:57:28 +0100

    feat(stores): channel store

--- src/lib/wyvern/src/react/non-mana/channel/ChannelCreator.tsx
+++ src/lib/wyvern/src/react/non-mana/channel/ChannelCreator.tsx
@@ -1,4 +1,8 @@
 // import { useEffect } from "react";
+const useChannels = (
+  await import("../../../scripts/stores/channel-store/ChannelStore")
+).default;
+
 import { useCurrentRoute } from "../../../scripts/stores/current-store/CurrentStore";
 
 const useGuildStore = (
@@ -6,18 +10,26 @@ const useGuildStore = (
 ).default;
 const ChannelButton = (await import("../buttons/ChannelButton")).default;
 
-export default function ChannelCreator(/* { guild_id }: { guild_id: string } */) {
-  const location = useCurrentRoute()
-  const id = location ? location.guild.id : "@jk"
-  const channels = useGuildStore().find(
-    (guild) => guild.id === (id),
-  )?.channels;
-  
+export default function ChannelCreator(
+  /* { guild_id }: { guild_id: string } */
+) {
+  const location = useCurrentRoute();
+  const channels2 = useChannels();
+  console.log(channels2)
+  const id = location ? location.guild.id : "@jk";
+  const channels = useGuildStore().find((guild) => guild.id === id)?.channels;
+
   return (
     <>
       {channels?.map((channel) => {
-        console.log("creating button for", channel.id, location.guild.id)
-        return <ChannelButton key={channel.id} channel={channel} guild={location.guild} />;
+        console.log("creating button for", channel.id, location.guild.id);
+        return (
+          <ChannelButton
+            key={channel.id}
+            channel={channel}
+            guild={location.guild}
+          />
+        );
       })}
     </>
   );
--- src/lib/wyvern/src/scripts/stores/channel-store/ChannelStore.tsx
+++ src/lib/wyvern/src/scripts/stores/channel-store/ChannelStore.tsx
@@ -1,17 +1,31 @@
-// // To make a channel store we need to store a list of channels
-// // indexed by their guild id
+// To make a channel store we need to store a list of channels
+// indexed by their guild id
 
-// import { useState } from "react";
-// import EventSystem from "../../core/EventSystem";
+import { useEffect, useState } from "react";
+import EventSystem from "../../core/EventSystem";
+import type { ReadyEvent } from "../../types/ReadyType";
+import type { Guild } from "../../types/GuildTypes";
+import type { Entry } from "../../types/Generics";
+import type { Channel } from "../../types/ChannelTypes";
 
-// export function useChannelStore() {
-//   const [channelData, setChannelData] = useState({})
-  
-//   EventSystem.on("CHANNEL_CREATE", (data) => {
-    
-//   })
-// }
+function entriesOfChannels(guilds: Guild[]): Entry<string, Channel[]>[] {
+  return guilds.map((guild) => {
+    return [guild.id, guild.channels];
+  });
+}
 
-// export default function getGuildChannels(id: string) {
-  
-// }
+export default function useChannels() {
+  const [channelData, setChannelData] = useState({});
+
+  useEffect(() => {
+    // We need to get every channel and organise them by guild ID
+    // like {[guild]:{[channels]}}
+    EventSystem.once("READY", (data: ReadyEvent) => {
+      const channelData = Object.fromEntries(entriesOfChannels(data.guilds));
+      console.log("channel data:",channelData,entriesOfChannels(data.guilds))
+      setChannelData(channelData)
+    });
+  }, []);
+
+  return channelData
+}
