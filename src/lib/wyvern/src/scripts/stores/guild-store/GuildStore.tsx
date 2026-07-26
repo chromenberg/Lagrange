e941f82edfc3136647f32118142b8e804a12d390
chromenberg <kalelmcevoy@hotmail.com>
2026-07-26 23:57:05 +0100

    fix(guildstore): fix ready event leak changes EventSystem.on -> EventSystem.once

--- src/lib/wyvern/src/scripts/stores/guild-store/GuildStore.tsx
+++ src/lib/wyvern/src/scripts/stores/guild-store/GuildStore.tsx
@@ -3,24 +3,17 @@ import type { GuildStore } from "../StoreTypes";
 import EventSystem from "../../core/EventSystem";
 // import type { GuildMemberUpdateData } from "./GuildUpdateType";
 import type { ReadyEvent } from "../../types/ReadyType";
+
 function useGuildStore() {
   const [guildStore, setGuildStore] = useState<GuildStore>([]);
 
   useEffect(() => {
-    EventSystem.on("READY", async (data: ReadyEvent) => {
+    EventSystem.once("READY", async (data: ReadyEvent) => {
       setGuildStore(data.guilds)
       console.log(data.guilds)
     });
   }, []);
-  // const id = userStore()
-  // const addGuild = (data: GuildMemberUpdateData  ) => {
-  //   if (data.user.id === )
-  // }
-
-  // const removeGuild = (data: GuildMemberUpdateData ) => {
-
-  // }
-
+  
   EventSystem.on("GUILD_MEMBER_ADD", (data) => {
     console.log(data);
   });
