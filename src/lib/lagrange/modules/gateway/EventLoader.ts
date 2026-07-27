27a9d08865d4959e0d4a8dd8200a16fdea63d387
chromenberg <kalelmcevoy@hotmail.com>
2026-07-27 02:37:43 +0100

    start event loader system

---
+++ src/lib/lagrange/modules/gateway/EventLoader.ts
@@ -0,0 +1,23 @@
+const RegisteredEvents = {}
+
+export namespace Events {
+  export function Load() {
+    
+  }
+
+  export function Unload() {
+    
+  }
+
+  export function List() {
+    
+  }
+
+  export function Update() {
+    
+  }
+
+  export function UnloadAll() {
+    
+  }
+}
