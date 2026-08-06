6d8081c829043c2d4e92d4e5cee5aaa6fabae9df
chromenberg <kalelmcevoy@hotmail.com>
2026-08-06 21:04:16 +0100

    chore: styling types

--- src/lib/wyvern/src/scripts/types/SizingTypes.ts
+++ src/lib/wyvern/src/scripts/types/SizingTypes.ts
@@ -1,3 +1,16 @@
-export type SizingName = "xsmall" | "small" | "medium" | "large" | "xlarge"
-export type Direction = "horizontal" | "vertical"
-export type Thickness = "vthin" | "thin" | "thick" | "vthick"
+export type SizingName = "xsmall" | "small" | "medium" | "large" | "xlarge";
+export type Direction = "horizontal" | "vertical";
+export type Thickness = "vthin" | "thin" | "thick" | "vthick";
+export type StyleLevel = "primary" | "secondary" | "tertiary";
+export type BorderStyle =
+  "subtle" | "strong" | "darken" | "lighten" | StyleLevel;
+export type ColoringStyles =
+  | "danger"
+  | "success"
+  | StyleLevel
+  | "profile-primary"
+  | "profile-secondary"
+  | "profile-tertiary"
+  | "neutral";
+export type SurfaceStyles = `${ColoringStyles}-surface`;
+export type AllColoringStyles = ColoringStyles | SurfaceStyles;
