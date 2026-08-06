abf863cfe5ab0c0c7fe4190206cde84d42795ad8
chromenberg <kalelmcevoy@hotmail.com>
2026-08-06 21:02:05 +0100

    feat: settings control component

--- src/lib/wyvern/src/react/mana/settings/SettingsControl.tsx
+++ src/lib/wyvern/src/react/mana/settings/SettingsControl.tsx
@@ -1,12 +1,11 @@
 import type { Props } from "../../../Core";
+import Stack from "../stack/Stack";
 import("./SettingsControl.css")
 
-interface SettingControlProps extends Props {
-}
 
 export default function SettingControl({
   children,
-}: SettingControlProps) {
+}: Props) {
  
   return (
     <div className={`settingsControl`}>
@@ -14,3 +13,23 @@ export default function SettingControl({
     </div>
   );
 }
+SettingControl.Labels = function Labels({children}: Props) {
+  return <div className="controlLabelContainer">{children}</div>
+}
+SettingControl.Label = function Label({children}: Props) {
+  return <div className="controlLabel">{children}</div>
+}
+
+SettingControl.Controls = function Controls({children}: Props) {
+  return <div className="controlContainer">{children}</div>
+}
+SettingControl.Control = function Control({children}: Props) {
+  return <div className="settingControl">
+    <Stack justify="end" align="center" gap="medium">
+      {children}
+    </Stack>
+  </div>
+}
+
+
+// export default SettingControl
