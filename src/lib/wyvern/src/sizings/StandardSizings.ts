af3551eff099fb4fe704b21adc174023c4295e80
chromenberg <kalelmcevoy@hotmail.com>
2026-08-06 21:03:58 +0100

    chore: update standard sizings

--- src/lib/wyvern/src/sizings/StandardSizings.ts
+++ src/lib/wyvern/src/sizings/StandardSizings.ts
@@ -1,9 +1,9 @@
 const StandardSizings = {
-  xs: "2px",
-  sm: "4px",
-  m: "8px",
-  lg: "12px",
-  ml: "16px",
-  xl:"24px"
-}
+  xsmall: 4,
+  small: 8,
+  medium: 16,
+  large: 24,
+  xlarge: 32,
+  xxlarge: 64,
+};
 export default StandardSizings;
