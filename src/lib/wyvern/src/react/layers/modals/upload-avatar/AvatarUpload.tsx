039d5ed73cbf0ad733e781096ad2790a73467aa8
chromenberg <kalelmcevoy@hotmail.com>
2026-08-10 17:24:33 +0100

    feat(frontend): [modals] avatar upload base

--- src/lib/wyvern/src/react/layers/modals/upload-avatar/AvatarUpload.tsx
+++ src/lib/wyvern/src/react/layers/modals/upload-avatar/AvatarUpload.tsx
@@ -1,12 +1,30 @@
+import CloseButton from "../../../mana/button/subtypes/Close";
 import Modal from "../Modal";
 import ModalView from "../ModalView";
+import("./AvatarUpload.css");
+import("../../../styles/Header.css");
 
-export default function AvatarUploadModal() {
+export default function AvatarUploadModal({
+  state,
+}: {
+  state?: (arg: boolean) => void;
+}) {
   return (
     <ModalView>
       <div className="modalContainerInner">
         <Modal style="modal" classes="modalType1">
-          sdfgsdfg
+          <div className="panel_avatarUpload">
+            <header className="header_avatarUpload">
+              <div className="headerLayout">
+                <div className="headerMain">
+                  <h1 className="semibold heading-lg">Select an Image</h1>
+                </div>
+                <div className="headerLast_avatarUpload">
+                  <CloseButton callback={() => state?.(false)} />
+                </div>
+              </div>
+            </header>
+          </div>
         </Modal>
       </div>
     </ModalView>
