c41ac7b6f87640adeaf9ad9d04f0bb5ee70c91ec
chromenberg <kalelmcevoy@hotmail.com>
2026-08-17 20:56:32 +0100

    feat(templates): emit sync events

--- src/lib/atlas/modules/templates/GuildTemplate.ts
+++ src/lib/atlas/modules/templates/GuildTemplate.ts
@@ -1,5 +1,7 @@
+import { Atlas } from "../../../../_Init.js";
 import type { ChannelData, GuildData } from "../../../core/types/FeatureTypes.js";
 import { Templates, type Snowflake } from "../../../core/types/Types.js";
+import { AtlasEvents } from "../../AtlasEvents.js";
 import { BaseTemplate } from "./BaseTemplate.js";
 
 export class GuildTemplate extends BaseTemplate<GuildData> {
@@ -12,5 +14,10 @@ export class GuildTemplate extends BaseTemplate<GuildData> {
     this.setValue("roles", [
       Templates.roles.everyone
     ])
+
+    Atlas.emit(AtlasEvents.guildCreate, ownerID, guildID)
+    startChannel.forEach(channel => {
+      Atlas.emit(AtlasEvents.channelCreate, ownerID, guildID, channel.id)
+    })
   }
 }
