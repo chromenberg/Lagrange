ce60f5a51d4b82f6ace8dc23f65240fc4d811c60
chromenberg <kalelmcevoy@hotmail.com>
2026-08-17 16:41:19 +0100

    feat(events): message create remake

--- src/lib/lagrange/modules/events/MessageCreate.ts
+++ src/lib/lagrange/modules/events/MessageCreate.ts
@@ -1,31 +1,36 @@
+import { channel } from "node:process";
+import { Atlas, Gateway } from "../../../../_Init.js";
 import { Logger, LogLevel } from "../../../core/logging/Logger.js";
+import type { Snowflake } from "../../../core/types/Types.js";
+import type { Member } from "../../../core/types/UserTypes.js";
 import { eventPublisher } from "../../services/EventService.js";
 import type { ClientConnection } from "../gateway/Connection.js";
-// import { GatewayEmitter } from "../gateway/Gateway.js";
-import {
-  GatewayEvent,
-  GatewayEventOpCodes,
-  GatewayEventTypes,
-} from "./GatewayEvents.js";
-// console.log(GatewayEventTypes.MESSAGE_CREATE)
-eventPublisher.subscribe("MESSAGE_CREATE", (data) => {
-  console.log(data);
-});
+import { GatewayEventTypes } from "./GatewayEvents.js";
 
-export function subscribeToMessages(guildChannel: any, sock: ClientConnection) {
-  console.log(guildChannel.guild_id?.toString(), guildChannel.channel_id?.toString())
-  // GatewayEmitter.channelSubscribe(
-  //   "MESSAGE_CREATE",
-  //   guildChannel.guild_id?.toString() as string,
-  //   guildChannel.channel_id?.toString() as string,
-  //   (msg: any) => {
-  //     sock.send(
-  //       JSON.stringify({
-  //         opCode: GatewayEventOpCodes.DISPATCH,
-  //         eventName: GatewayEventTypes.MESSAGE_CREATE,
-  //         data: msg,
-  //       }),
-  //     );
-  //   },
-  // );
+interface MessageCreateData {
+  guild: Snowflake;
+  channel: Snowflake;
+  // we use member as guilds exist
+  author: Partial<Member>
+  content: string;
 }
+
+Gateway.addEvent(GatewayEventTypes.MESSAGE_CREATE, (data: MessageCreateData) => {
+  console.log("bemssage")
+  // get message id then format the data to the message events data
+  const messageID = Atlas.requests.messages.requestMessageID().toString()
+  const formatted = {
+    author: {
+      author_id: data.author.user?.id,
+      username: data.author.user?.username,
+      display_name: data.author.user?.display_name,
+      user: data.author.user
+    },
+    channel: data.channel,
+    message_id: messageID,
+    content: data.content
+  }
+
+  // send the gateway event
+  Gateway.channelEvent(data.guild, data.channel, GatewayEventTypes.MESSAGE_CREATE, formatted)
+})
