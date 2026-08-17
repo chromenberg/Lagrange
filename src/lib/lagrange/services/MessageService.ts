a3e40f48abca13327f3e7370c023272259a9da14
chromenberg <kalelmcevoy@hotmail.com>
2026-08-17 16:39:26 +0100

    feat(gateway): remove old message events

--- src/lib/lagrange/services/MessageService.ts
+++ src/lib/lagrange/services/MessageService.ts
@@ -4,7 +4,7 @@ import type { Request } from "../modules/rest/Router.js";
 import { pubSub } from "../modules/gateway/PubSubHandler.js";
 import { HTTPReader } from "../modules/rest/HTTPReader.js";
 import { emitEvent } from "./EventService.js";
-import { Atlas } from "../../../_Init.js";
+import { Atlas, Gateway } from "../../../_Init.js";
 import { Registry } from "../registries/LagrangeRegistry.js";
 // import { GatewayEmitter } from "../modules/gateway/Gateway.js";
 
@@ -67,24 +67,15 @@ export class MessageService {
           const channelID: string = req.params.id;
           const guildID: string =
             Registry.fetch("inverseChannelMap")?.get(channelID);
-
-          // a message create event should have a body, a channel id and an auth token
-          // console.log(GatewayEmitter.getChannel(guildID, channelID))
           console.log(`Sent in guild ${guildID} in channel ${channelID}`)
-          // console.log(GatewayEmitter.getChannel(guildID, channelID)?.listeners("MESSAGE_CREATE"))
-          // GatewayEmitter.getChannel(guildID, channelID)?.publish(
-          //   "MESSAGE_CREATE",
-          //   {
-          //     author: {
-          //       author_id: user.user_id,
-          //       username: user.username,
-          //       display_name: user.display_name,
-          //     },
-          //     channel_id: req.params.id, // we need to check if the user can access this
-          //     message_id: Atlas.requests.messages.requestMessageID().toString(),
-          //     content: body.content,
-          //   },
-          // );
+          
+          Gateway.emitEvent("MESSAGE_CREATE", {
+            guild: guildID,
+            channel: channelID,
+            author: user,
+            content: body.content
+          })
+          
           res.statusCode = 200;
           res.write("was this the fix?");
           res.end();
