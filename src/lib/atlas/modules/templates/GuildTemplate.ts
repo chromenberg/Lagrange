e5e6720f0d8f1102b4b9b36af4391a1546a22c38
chromenberg <kalelmcevoy@hotmail.com>
2026-08-19 13:27:18 +0100

    chore(templates): guild template closer to guild type

--- src/lib/atlas/modules/templates/GuildTemplate.ts
+++ src/lib/atlas/modules/templates/GuildTemplate.ts
@@ -1,23 +1,34 @@
 import { Atlas } from "../../../../_Init.js";
-import type { ChannelData, GuildData } from "../../../core/types/FeatureTypes.js";
+import type {
+  ChannelData,
+  GuildData,
+} from "../../../core/types/FeatureTypes.js";
 import { Templates, type Snowflake } from "../../../core/types/Types.js";
 import { AtlasEvents } from "../../AtlasEvents.js";
 import { BaseTemplate } from "./BaseTemplate.js";
 
 export class GuildTemplate extends BaseTemplate<GuildData> {
-  constructor(guildID: Snowflake, ownerID: Snowflake, startChannel: ChannelData[], name: string) {
+  constructor(
+    guildID: Snowflake,
+    ownerID: Snowflake,
+    startChannel: ChannelData[],
+    name: string,
+  ) {
+    // this has the type of Guild
     super();
-    this.setValue("id", guildID)
-    this.setValue("owner_id", ownerID)
-    this.setValue("name", name)
-    this.setValue("channels", startChannel)
-    this.setValue("roles", [
-      Templates.roles.everyone
-    ])
+    this.setValue("id", guildID);
+    // @ts-ignore
+    this.setValue("properties", {
+      name: name,
+      owner_id: ownerID,
+      icon: null,
+    });
+    this.setValue("channels", startChannel);
+    this.setValue("roles", [Templates.roles.everyone]);
 
-    Atlas.emit(AtlasEvents.guildCreate, ownerID, guildID)
-    startChannel.forEach(channel => {
-      Atlas.emit(AtlasEvents.channelCreate, ownerID, guildID, channel.id)
-    })
+    Atlas.emit(AtlasEvents.guildCreate, ownerID, guildID);
+    startChannel.forEach((channel) => {
+      Atlas.emit(AtlasEvents.channelCreate, ownerID, guildID, channel.id);
+    });
   }
 }
