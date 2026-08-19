b46efa071c718451355a8665524d5f0131134f13
chromenberg <kalelmcevoy@hotmail.com>
2026-08-19 13:19:55 +0100

    feat(frontend): [Services]: add event emit for guild create

--- src/lib/lagrange/services/GuildService.ts
+++ src/lib/lagrange/services/GuildService.ts
@@ -4,11 +4,12 @@ import type { Request } from "../modules/rest/Router.js";
 import { emitEvent, pubSub } from "../modules/gateway/PubSubHandler.js";
 import { HTTPReader } from "../modules/rest/HTTPReader.js";
 import type { GuildCreateObj } from "../interfaces/Guilds.js";
-import { Atlas } from "../../../_Init.js";
+import { Atlas, Gateway } from "../../../_Init.js";
 import type { GuildData } from "../../core/types/FeatureTypes.js";
 import type { Snowflake } from "../../core/types/Types.js";
 // import { GatewayEmitter } from "../modules/gateway/Gateway.js";
 import type { Server } from "ws";
+import { GatewayEventTypes } from "../modules/events/GatewayEvents.js";
 
 export class GuildService {
   private readonly route: Route;
@@ -22,7 +23,7 @@ export class GuildService {
     // -----------------------
 
     this.route.post("/", (req, res) => {
-      console.log("sddfsg")
+      console.log("sddfsg");
       this.newGuild(req, res);
     });
 
@@ -31,58 +32,66 @@ export class GuildService {
     });
 
     this.route.get("/:id/channels", (req, res) => {
-      this.getGuildChannels(req, res)
-    })
+      this.getGuildChannels(req, res);
+    });
   }
 
   // should have a guild name as of right now
   public async newGuild(req: Request, res: ServerResponse<IncomingMessage>) {
-    console.log("gdfgdfgsdf")
-    HTTPReader.parseBody(req).then((body) => {
+    console.log("gdfgdfgsdf");
+    HTTPReader.parseBody<GuildData>(req).then((body) => {
       console.log(body);
       // TODO: Clean this up
+      // TODO: Rewrite the request system, new method to check if the body has certain fields
       if (
         Object.hasOwn(body as Record<string, any>, "id") &&
         Object.hasOwn(body as Record<string, any>, "name")
       ) {
         console.log("new guild");
-        (body as GuildData).owner_id = atob(
-          req.headers.authorization?.split(".")[0] ?? "",
-        );
-        (body as GuildData).channels = [];
-        (body as GuildData).roles = [];
-        Atlas.requests.guilds.newGuild(body as GuildData);
+        // FIX: An invalid token could slip through and cause this to break
+        body.owner_id = atob(req.headers.authorization?.split(".")[0] ?? "");
+        body.channels = [];
+        body.roles = [];
+
+        Atlas.requests.guilds.newGuild(body).then((guild) => {
+          // Send the guild create event to the owner id
+          // Event type Guild Create
+          // with the data of the guild that was made
+          Gateway.userEvent(
+            body.owner_id,
+            GatewayEventTypes.GUILD_CREATE,
+            guild,
+          );
+        });
       }
-      res.write("dfgdfg")
-      res.end()
+      res.write("dfgdfg");
+      res.end();
     });
   }
 
   public newInviteCode(req: Request, res: ServerResponse<IncomingMessage>) {
-    const code = Atlas.requests.guilds.newInvite(req.params.id)
-    res.setHeader("content-type", "application/json")
-    res.write(JSON.stringify({invite_code: code}))
-    res.end()
+    const code = Atlas.requests.guilds.newInvite(req.params.id);
+    res.setHeader("content-type", "application/json");
+    res.write(JSON.stringify({ invite_code: code }));
+    res.end();
   }
 
-  public addGuildMember(guildID: Snowflake, userID: Snowflake) {
-
-  }
+  public addGuildMember(guildID: Snowflake, userID: Snowflake) {}
 
   public getGuildChannels(req: Request, res: ServerResponse<IncomingMessage>) {
     Atlas.requests.guilds.getChannels(req.params.id).then((channels) => {
-      const channelMap = channels?.map(channel => {
+      const channelMap = channels?.map((channel) => {
         return {
           id: channel.channel_id?.toString(),
           name: channel.channel_name,
           index: channel.channel_index?.toString(),
-          type: "text"
-        }
-      })
+          type: "text",
+        };
+      });
 
-      res.setHeader("Content-Type", "application/json")
-      res.write(JSON.stringify(channelMap))
-      res.end()
-    })
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(channelMap));
+      res.end();
+    });
   }
 }
