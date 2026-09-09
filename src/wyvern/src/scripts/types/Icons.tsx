4dcedd2b526b8e48f5ba24db85866865e5d2e024
Raine <kalelmcevoy@hotmail.com>
2026-09-09 12:45:21 +0100

    feat(icons): add search icon

--- src/wyvern/src/scripts/types/Icons.tsx
+++ src/wyvern/src/scripts/types/Icons.tsx
@@ -1,5 +1,6 @@
 import { lazy } from "react";
 
 export const Icons = {
-  "exit-cross": lazy(() => import("../../svg-icons/ExitCross"))
-}
+  "exit-cross": lazy(() => import("../../svg-icons/ExitCross")),
+  search: lazy(() => import("../../svg-icons/Search")),
+};
