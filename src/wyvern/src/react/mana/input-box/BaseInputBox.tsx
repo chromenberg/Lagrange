bb9f576b9b8399a6d91efdb0374e6544c59e63ab
Raine <kalelmcevoy@hotmail.com>
2026-09-10 09:08:12 +0100

    feat(mana): make base input box fill height

--- src/wyvern/src/react/mana/input-box/BaseInputBox.tsx
+++ src/wyvern/src/react/mana/input-box/BaseInputBox.tsx
@@ -5,12 +5,11 @@ export default function BaseInputBox({ children, className }: Partial<Props>) {
   // An input-box type can be chosen by entering a parameter in this component
 
   return (
-    <div mana-type="input-box" className={"manaInputBoxContainer" + (className ? " " + className : "")}>
-      <Stack
-        align="center"
-        gap="xsmall"
-        className={"manaInputBox"}
-      >
+    <div
+      mana-type="input-box"
+      className={"manaInputBoxContainer" + (className ? " " + className : "")}
+    >
+      <Stack fillHeight align="center" gap="xsmall" className={"manaInputBox"}>
         {children}
       </Stack>
     </div>
