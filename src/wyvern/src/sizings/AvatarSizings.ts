aaa21576f0fdfb93b8512eba106e2f77d2d9f78e
Raine <kalelmcevoy@hotmail.com>
2026-09-10 12:50:01 +0100

    feat(sizings): add pile sizings

--- src/wyvern/src/sizings/AvatarSizings.ts
+++ src/wyvern/src/sizings/AvatarSizings.ts
@@ -1,6 +1,13 @@
- const AvatarSizing = {
+const AvatarSizing = {
+  Pile: {
+    xsmall: 16,
+    small: 24,
+    medium: 28,
+    large: 32,
+    xlarge: 48,
+  },
   Message: {
-    size: 40
-  }
- }
- export default AvatarSizing
+    size: 40,
+  },
+};
+export default AvatarSizing;
