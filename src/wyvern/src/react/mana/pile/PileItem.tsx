ac4477a13b3d9653888bb4a2f7ef42681877cc06
Raine <kalelmcevoy@hotmail.com>
2026-09-10 12:50:18 +0100

    Update PileItem.tsx

--- src/wyvern/src/react/mana/pile/PileItem.tsx
+++ src/wyvern/src/react/mana/pile/PileItem.tsx
@@ -1,7 +1,7 @@
 import type { Props } from "../../../Core";
 
 export default function PileItem({ children }: Partial<Props>) {
-  return <div mana-type="pile-item">
+  return <div mana-type="pile-item" className="pileItemWrapper">
     {children}
   </div>
 }
