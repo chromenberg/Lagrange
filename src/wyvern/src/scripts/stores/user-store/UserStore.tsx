f4611ca01a0c404a23576ec78fb114ec4be05c1d
Raine <kalelmcevoy@hotmail.com>
2026-09-10 12:22:37 +0100

    feat(stores): user store

--- src/wyvern/src/scripts/stores/user-store/UserStore.tsx
+++ src/wyvern/src/scripts/stores/user-store/UserStore.tsx
@@ -3,6 +3,7 @@ const EventSystem = (await import("../../core/EventSystem")).default;
 import type { User } from "../../types/UserTypes";
 import type { ReadyEvent } from "../../types/ReadyType";
 import { useEffect } from "react";
+import type { WeakObj } from "../../types/WeakObj";
 /* (data: ReadyEvent) => {
   console.log("[Stores/UserInfo] Synchronizing")
   setUserInfo({
@@ -13,17 +14,57 @@ import { useEffect } from "react";
     avatar: data.user.avatar,
     discriminator: data.user.discriminator
   } */
+
+// const affectableEvents: (keyof EventTypes)[] = [
+//   "GUILD_MEMBER_ADD",
+//   "GUILD_MEMBER_UPDATE",
+//   "USER_UPDATE",
+//   "PRESENCE_UPDATE",
+// ];
+
+// interface MemberAddEvent {
+
+// }
+
+type UserMap = {
+  [key: string]: Partial<User>;
+};
+
 // this is actually the clients info instead of the user, this is an incorrect name as the CLIENT refers to the current user
-function useUserInfo() {
-  const [userData, setUserInfo] = useState<Partial<User>>({});
+function useUserStore() {
+  const [userData, setUserInfo] = useState<UserMap>({});
+
+  // function memberAdd(hook: typeof setUserInfo, data: unknown) {
+  //   setUserInfo;
+  // }
+
+  // const eventCallbacks = [(data) => {}];
   useEffect(() => {
-    EventSystem.once("GUILD_MEMBER_ADD");
-    EventSystem.once("");
+    function update(data: WeakObj) {
+      setUserInfo({
+        ...userData,
+        [data.user.id]: {
+          username: data.user.username,
+          display_name: data.user.display_name,
+          id: data.user.id,
+          avatar: data.user.avatar,
+          discriminator: data.user.discriminator,
+        },
+      });
+    }
+    console.log("Updating user store");
+    EventSystem.once("GUILD_MEMBER_ADD", (data: WeakObj) => {
+      update(data);
     });
-    
-  }, []);
-  console.log(userData)
+    EventSystem.once("USER_UPDATE", (data: WeakObj) => {
+      update(data);
+    });
+    EventSystem.once("READY", (data: ReadyEvent) => {
+      update(data);
+    });
+  }, [userData]);
+  console.log(userData);
   return userData;
 }
 
-export default useUserInfo;
+export default useUserStore;
