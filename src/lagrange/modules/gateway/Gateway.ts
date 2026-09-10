ad43b7aaba84d0a9fcce35fd7a4ab65d6a210627
Raine <kalelmcevoy@hotmail.com>
2026-09-10 09:07:52 +0100

    chore(gateway): formatting

--- src/lagrange/modules/gateway/Gateway.ts
+++ src/lagrange/modules/gateway/Gateway.ts
@@ -1,5 +1,9 @@
 import EventEmitter from "events";
-import type { Snowflake, VoidCallback, WeakObj } from "../../../core/types/Types.js";
+import type {
+  Snowflake,
+  VoidCallback,
+  WeakObj,
+} from "../../../core/types/Types.js";
 import { nextTick } from "process";
 import { CallArray } from "../../../core/structs/CallArray.js";
 import { EventSystem } from "./EventHandler.js";
@@ -15,7 +19,6 @@ import { Logger, LogLevel } from "../../../core/logging/Logger.js";
 import { Registry } from "../../registries/LagrangeRegistry.js";
 import { Atlas } from "../../../_Init.js";
 
-
 class ModuleLoader {
   constructor() {}
 
@@ -24,60 +27,64 @@ class ModuleLoader {
   public unload() {}
 }
 
-
-
-
-export class GatewayEventHandler {
-  
-}
+export class GatewayEventHandler {}
 export class Gateway {
   private _eventPublisher: EventSystem;
   private _gatewayEvents: EventEmitter;
   private _socket: WebSocketServer;
 
   constructor(eventPublisher?: EventSystem, gatewayEvents?: EventEmitter) {
-    this._eventPublisher = eventPublisher ?? new EventSystem()
-    this._gatewayEvents = gatewayEvents ?? new EventEmitter()
+    this._eventPublisher = eventPublisher ?? new EventSystem();
+    this._gatewayEvents = gatewayEvents ?? new EventEmitter();
     this._socket = new WebSocketServer({
       port: Config.Gateway.Socket.Port,
       host: "127.0.0.1",
     });
 
-    this.initConnectionHandler()
-    console.log("started gateway")
+    this.initConnectionHandler();
+    console.log("started gateway");
     nextTick(() => {
-      Logger.sendLog(LogLevel.Success, ["LAGRANGE", "Gateway"], "Initialized")
-      process.emit("gatewayInit")
-    })
+      Logger.sendLog(LogLevel.Success, ["LAGRANGE", "Gateway"], "Initialized");
+      process.emit("gatewayInit");
+    });
   }
 
   // -- Subscribe --
 
   public guildSubscribe(guildID: Snowflake, listener: VoidCallback) {
-    this._eventPublisher.guildSub(guildID, listener)
+    this._eventPublisher.guildSub(guildID, listener);
   }
 
-  public channelSubscribe(guildID: Snowflake, channelID: Snowflake, listener: VoidCallback) {
-    this._eventPublisher.channelSub(guildID, channelID, listener)
+  public channelSubscribe(
+    guildID: Snowflake,
+    channelID: Snowflake,
+    listener: VoidCallback,
+  ) {
+    this._eventPublisher.channelSub(guildID, channelID, listener);
   }
 
   public userSubscribe(userID: Snowflake, listener: VoidCallback) {
-    this._eventPublisher.userSub(userID, listener)
-  }
-  
-  // -- Emitter Methods --
-  
-  public guildEvent(guildID: Snowflake, eventName: string, data: WeakObj) {
-    this._eventPublisher.emitGuild(guildID, eventName, data)
+    this._eventPublisher.userSub(userID, listener);
   }
 
-  public channelEvent(guildID: Snowflake, channelID: Snowflake, eventName: string, data: WeakObj) {
-    this._eventPublisher.emitChannel(guildID, channelID, eventName, data)
+  // -- Emitter Methods --
+
+  public guildEvent(guildID: Snowflake, eventName: string, data: WeakObj) {
+    this._eventPublisher.emitGuild(guildID, eventName, data);
+  }
+
+  public channelEvent(
+    guildID: Snowflake,
+    channelID: Snowflake,
+    eventName: string,
+    data: WeakObj,
+  ) {
+    this._eventPublisher.emitChannel(guildID, channelID, eventName, data);
   }
 
   public userEvent(userId: Snowflake, eventName: string, data: WeakObj) {
-    console.log(userId, eventName, data)
-    this._eventPublisher.emitUser(userId, eventName, data)
+    console.log(userId, eventName, data);
+    this._eventPublisher.emitUser(userId, eventName, data);
   }
   // -- WebSocket Methods --
 
@@ -90,7 +97,7 @@ export class Gateway {
 
   // Initializes a new client connection, starts the heartbeat loop and sends the hello event
   private createClientObj(sock: WebSocket) {
-    const client = new ClientConnection(sock)
+    const client = new ClientConnection(sock);
 
     // Add to heartbeat loop
     const socketID = ClientConnections.add(client, () => {
@@ -101,29 +108,27 @@ export class Gateway {
     client.setID(socketID[0]);
 
     // Send the hello event to the client
-    this._hello(client)
+    this._hello(client);
   }
 
-
-  
   /**
    * Adds a gateway event handler
-   * @param name 
-   * @param listener 
+   * @param name
+   * @param listener
    */
   public addEvent(name: KeyOfEvents, listener: VoidCallback) {
-    this._gatewayEvents.on(name, listener)
+    this._gatewayEvents.on(name, listener);
   }
 
   public emitEvent(name: KeyOfEvents, ...data: any[]) {
-    this._gatewayEvents.emit(name, ...data)
+    this._gatewayEvents.emit(name, ...data);
   }
-  
+
   // Init
-  
+
   public initConnectionHandler() {
-    this._socket.on("connection", sock => {
-      this.createClientObj(sock)
-    })
+    this._socket.on("connection", (sock) => {
+      this.createClientObj(sock);
+    });
   }
 }
