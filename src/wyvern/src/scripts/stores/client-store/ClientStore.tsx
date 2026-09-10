a6426663329b9517461ea9d7a1ef21232148b40e
Raine <kalelmcevoy@hotmail.com>
2026-09-10 11:54:13 +0100

    chore(store): rename userstore to clientstore

---
+++ src/wyvern/src/scripts/stores/client-store/ClientStore.tsx
@@ -0,0 +1,28 @@
+const { useState } = await import("react");
+const EventSystem = (await import("../../core/EventSystem")).default;
+import type { ClientUser } from "../../types/UserTypes";
+import type { ReadyEvent } from "../../types/ReadyType";
+import { useEffect } from "react";
+
+// this is actually the clients info instead of the user, this is an incorrect name as the CLIENT refers to the current user
+function useUserInfo() {
+  const [userData, setUserInfo] = useState<Partial<ClientUser>>({});
+  useEffect(() => {
+    EventSystem.once("READY", (data: ReadyEvent) => {
+      console.log("[Stores/UserInfo] Synchronizing")
+      setUserInfo({
+        username: data.user.username,
+        display_name: data.user.display_name,
+        id: data.user.id,
+        email: data.user.email,
+        avatar: data.user.avatar,
+        discriminator: data.user.discriminator
+      });
+    });
+    
+  }, []);
+  console.log(userData)
+  return userData;
+}
+
+export default useUserInfo;
