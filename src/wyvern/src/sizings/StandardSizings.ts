fd32c12abace2e42cd456937096b60fcbb5f3369
Raine <kalelmcevoy@hotmail.com>
2026-09-11 11:05:22 +0100

    feat(sizings): add pile overlap offsets

--- src/wyvern/src/sizings/StandardSizings.ts
+++ src/wyvern/src/sizings/StandardSizings.ts
@@ -1,4 +1,12 @@
 const StandardSizings = {
+  Pile: {
+    // follows [Size] * [Scale Factor], these values are the same, positive versions of their CSS values
+    xsmall: 4 * 2,
+    small: 8 * 1.6,
+    medium: 16 * 1,
+    large: 24 * 0.8,
+    xlarge: 32 * 0.8,
+  },
   xsmall: 4,
   small: 8,
   medium: 16,
