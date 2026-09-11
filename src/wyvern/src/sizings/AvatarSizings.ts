7d9df85e1fabf81dc4c563e14e9a0b825c5d077f
Raine <kalelmcevoy@hotmail.com>
2026-09-11 09:48:58 +0100

    feat(sizings): overlap factors

--- src/wyvern/src/sizings/AvatarSizings.ts
+++ src/wyvern/src/sizings/AvatarSizings.ts
@@ -1,9 +1,15 @@
 const AvatarSizing = {
   Pile: {
+    // The factor to divide the pile icon size by to get the correct clipping
+    OverlapFactors: {
+      close: 6,
+      medium: 8,
+      far: 10,
+    },
     xsmall: 16,
     small: 24,
     medium: 28,
-    large: 32,
+    large: 38,
     xlarge: 48,
   },
   Message: {
