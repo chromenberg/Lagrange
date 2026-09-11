6bf0fe43f10a0b92e27e332830a08ecbdccd84b9
Raine <kalelmcevoy@hotmail.com>
2026-09-11 10:58:58 +0100

    feat(mana): avatar pile with masking

--- src/wyvern/src/react/mana/pile/avatar/AvatarPile.tsx
+++ src/wyvern/src/react/mana/pile/avatar/AvatarPile.tsx
@@ -1,6 +1,7 @@
+import type { Props } from "../../../../Core";
 import useUserAvatar from "../../../../scripts/hooks/user/useUserAvatar";
 import type {
-  Direction,
+  // Direction,
   SizingName,
 } from "../../../../scripts/types/SizingTypes";
 import AvatarSizing from "../../../../sizings/AvatarSizings";
@@ -9,19 +10,69 @@ import PileItem from "../PileItem";
 
 import("./AvatarPile.css");
 import("../Pile.css");
+import("../PileOverlaps.css")
+type Overlap = keyof typeof AvatarSizing.Pile.OverlapFactors;
+
+type ClipProps = {
+  size: number;
+  willMask: boolean;
+  overlap: Overlap;
+} & Props;
+
+function PileClipWrapper({ children, size, willMask, overlap }: ClipProps) {
+  console.log(willMask);
+  return (
+    <svg
+      width={size}
+      height={size}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${size} ${size}`}
+      preserveAspectRatio="none"
+    >
+      <mask id="clipMask" type="luminance">
+        {/*
+          We draw all keep parts first and then remove them with the black draw pass
+          This ensures that the image is visible and we can clip out parts we dont want
+        */}
+        <circle cx={size / 2} cy={size / 2} r={size / 2} fill="white" />
+
+        <circle
+          cx={-(size / AvatarSizing.Pile.OverlapFactors[overlap]-2)}
+          cy={size / 2}
+          r={(size / 2)+2}
+          fill="black"
+        />
+      </mask>
+
+      {/*
+          If willMask is false then we do not carve out and outline for this avatar
+          Otherwise the first item would have an ugly indent and i dont want that
+
+          the CSS contains border-radius to keep everything round
+        */}
+      <foreignObject mask={willMask ? "url(#clipMask)" : ""}>
+        {children}
+      </foreignObject>
+    </svg>
+  );
+}
 
 type AvatarPileItemProps = {
   user: string;
   size: number;
+  index: number;
+  overlap: Overlap
 };
-
-function AvatarPileItem({ user, size }: AvatarPileItemProps) {
+  
+function AvatarPileItem({ user, size, index, overlap }: AvatarPileItemProps) {
   const avatar = useUserAvatar(user);
-  console.log(avatar);
+  const willMask = index !== 0;
   return (
-    <PileItem>  
+    <PileItem>
       <div className="pileAvatarContainer">
-        <img src={avatar} width={size} height={size} className="pileAvatar" />
+        <PileClipWrapper willMask={willMask} size={size} overlap={overlap}>
+          <img src={avatar} width={size} height={size} className="pileAvatar" />
+        </PileClipWrapper>
       </div>
     </PileItem>
   );
@@ -31,21 +82,30 @@ type AvatarPileProps = {
   users: string[]; // array containing all the user ids in the pile
   // maxLength: number;
   // placeholderLength: number;
-  // overlap: SizingName;
+  overlap?: SizingName;
   // direction: Direction;
   height: SizingName;
+  clipOverlap: Overlap;
 };
 
 function parseHeight(height: SizingName) {
   return AvatarSizing.Pile[height];
 }
 
-export default function AvatarPile({ users, height }: AvatarPileProps) {
+export default function AvatarPile({ users, height, clipOverlap, overlap }: AvatarPileProps) {
+  console.warn('[AvatarPile] Prop drilling present! parameter "overlap" is passed into 2 levels of components')
   return (
-    <Pile>
-      {users.map((id) => {
-        
-        return <AvatarPileItem user={id} size={parseHeight(height)} />;
+    <Pile data-pile-overlap={overlap}>
+      {users.map((id, i) => {
+        return (
+          <AvatarPileItem
+            key={id}
+            user={id}
+            size={parseHeight(height)}
+            index={i}
+            overlap={clipOverlap}
+          />
+        );
       })}
     </Pile>
   );
