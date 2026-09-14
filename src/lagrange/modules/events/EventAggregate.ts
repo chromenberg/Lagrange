8f8d6233716231178fa2fe9e229ae356bdf1cf48
Raine <kalelmcevoy@hotmail.com>
2026-09-14 14:46:02 +0100

    feat(events): member add

--- src/lagrange/modules/events/EventAggregate.ts
+++ src/lagrange/modules/events/EventAggregate.ts
@@ -2,5 +2,6 @@ import "./Heartbeat.js";
 import "./Identify.js";
 import "./MessageCreate.js";
 import "./Ready.js";
-import "./GuildCreate.js"
+import "./GuildCreate.js";
+import "./MemberAdd.js"
 export default void function initEvents() {};
--- src/lagrange/modules/events/MemberAdd.ts
+++ src/lagrange/modules/events/MemberAdd.ts
@@ -3,22 +3,31 @@ import type { GuildTemplate } from "../../../atlas/modules/templates/GuildTempla
 import { Logger, LogLevel } from "../../../core/logging/Logger.js";
 import type { Guild } from "../../../core/types/GuildTypes.js";
 import type { Snowflake } from "../../../core/types/Types.js";
-import type { Member } from "../../../core/types/UserTypes.js";
+import type { Member, User } from "../../../core/types/UserTypes.js";
 import { GatewayEventTypes } from "./GatewayEvents.js";
 
 interface GuildMemberAddEvent {
-  member: Member;
-  guild: Snowflake;
+  user: User;
+  guild: Guild;
 }
 
 process.on("gatewayInit", () => {
   Gateway.addEvent(GatewayEventTypes.GUILD_MEMBER_ADD, (data: GuildMemberAddEvent, eventName) => {
     Logger.sendLog(LogLevel.Verbose, ["Events"], eventName)
-    // send the gateway event
-    Gateway.guildEvent(
+
+
+    // This is sent to the user who is joining the guild
+    Gateway.userEvent(
+      data.user.id,
+      GatewayEventTypes.GUILD_CREATE,
       data.guild,
+    );
+    
+    // This is sent to the members who are already in the guild
+    Gateway.guildEvent(
+      data.guild.id,
       eventName,
-      data.member,
+      data.user,
     );
   });
 });
